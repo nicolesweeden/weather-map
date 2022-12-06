20173889525e8e7a39e6c528b5c5cd011a8bbf68
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-12-06 03:01:09 -0700

    finishing up project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,4 +1,6 @@
-"use strict";
+(function() {
+    "use strict";
+    $(document).ready(function() {
 
 
 $.get("http://api.openweathermap.org/data/2.5/forecast", {
@@ -9,14 +11,24 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
     console.log('5 day forecast', data);
 });
 
+
 let lat = 39.7392;
 let long = -104.9903;
 $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + API_KEY + "&units=imperial").done(function(data) {
     let reports = data.list;
+    let html = "";
     for(let i = 0; i < reports.length; i += 8) {
         // should get 5 objects back
         console.log(reports[i]);
+        html += "<div id='cards' class='col-md-2'>";
+        html += "<p>Current temperature of " + reports[i].main.temp + "°F" + "</p>";
+        html += "<p>High of " + reports[i].main.temp_max + "°F" + "</p>";
+        html += "<p>Low of " + reports[i].main.temp_min + "°F" + "</p>";
+        html += "</div>";
+
     }
+    // console.log(html);
+    $("#container").html(html);
 });
 
 mapboxgl.accessToken = MAPBOX_APPID;
@@ -30,14 +42,12 @@ var map = new mapboxgl.Map({
 var marker = new mapboxgl.Marker({
     draggable: true,
     color: "#c71585"
+
 })
     .setLngLat([-104.9893, 39.7372])
     .addTo(map);
 
 
-// var marker = new mapboxgl.Marker({
-//     draggable: true,
-//     color: "#9E7E44"
-// })
-//     .setLngLat(ll)
-//     .addTo(map);
+// End of document ready
+});
+})();
